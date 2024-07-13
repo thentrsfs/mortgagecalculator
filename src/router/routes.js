94b5e88a69daf7e79497b5c9434f0523fede96c0
thentrsfs <filipstojkov54@gmail.com>
2024-07-13 16:34:18 +0200

    adadada

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,13 +2,6 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue')
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
   }
 ]
 
